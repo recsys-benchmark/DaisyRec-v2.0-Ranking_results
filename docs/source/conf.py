0ce27678adeff8d958be0ebeee3dfb8f679b259c
hylll <631774934@qq.com>
2022-05-17 15:05:04 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Da'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DaisyRec-v2.0'
+copyright = '2022, DaisyRec-v2.0'
+author = 'DaisyRec Contirbutors'
 
 release = '0.1'
 version = '0.1.0'
